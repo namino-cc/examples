6671674042ddda7e4edfbef577482ce9bfd23e23
Andrea Sansoe <asansoe@mect.it>
2023-09-26 17:46:50 +0200

    namino_rfid_access: Fixed used SPI Pins

--- Arduino/namino_arancio_magic_box/namino_arancio_magic_box.ino
+++ Arduino/namino_arancio_magic_box/namino_arancio_magic_box.ino
@@ -1,4 +1,6 @@
 #include <namino_rosso.h>
+#include <SPI.h>
+
 #include <MFRC522.h>
 
 #ifdef NAMINO_ROSSO_BOARD
@@ -10,11 +12,25 @@
 #define LINE_LEN    20              // Line Buffer Size
 
 // Namino Rosso interface instance
-namino_rosso nr = namino_rosso();
+// namino_rosso nr = namino_rosso();
+
+// SPI Interface
+#define VSPI FSPI
+SPIClass spi;
+
 
 // RC522 Reader
-#define RST_PIN 18
-MFRC522 mfrc522(SS, RST_PIN);   // Create MFRC522 instance.
+#define MFRC522_SPICLOCK (800000U)	// MFRC522 accept upto 10MHz, set to 800KHz.
+#define RST_PIN       (18)
+#define MOSI_PIN      MOSI        //11
+#define MISO_PIN      MISO        // 13
+#define CLK_PIN       SCK         // 12
+#define CS_PIN        CS_SDCARD   //2
+#define NM_SS         (10)
+
+
+
+MFRC522 mfrc522(CS_PIN, RST_PIN);   // Create MFRC522 instance.
 MFRC522::MIFARE_Key key;
 
 // Loop Params
@@ -26,22 +42,48 @@ void setup() {
   Serial.begin(115200);
   while (!Serial);
   Serial.println("");
-  // Namino Industrial Interface start
-  Serial.println("Namino Rosso Interface starting");
-  // reset namino microcontroller. Industrial side board reset.
-  nr.resetSignalMicroprocessor();
-  // Openin communication of the industrial side interface
-  nr.begin(800000U, MISO, MOSI, SCK, SS);
+  Serial.println("=====================================");
+  Serial.println("##      RFID MAGIC BOX EXAMPLE     ##");
+  Serial.println("=====================================");
+  Serial.println();
+  Serial.print("MOSI: ");
+  Serial.println(MOSI);
+  Serial.print("MISO: ");
+  Serial.println(MISO);
+  Serial.print("SCK: ");
+  Serial.println(SCK);
+  Serial.print("CS: ");
+  Serial.println(CS_SDCARD);  
   delay(2000);
-  nr.readAllRegister();
+  // TODO: Start Mamino Industrial Interface
+  // Namino Industrial Interface start
+  // Serial.println("Namino Rosso Interface starting");
+  // reset namino microcontroller. Industrial side board reset.
+  // nr.resetSignalMicroprocessor();
+  // Openin communication of the industrial side interface
+  // nr.begin(800000U, MISO, MOSI, SCK, SS);
+  // delay(2000);
+  // nr.readAllRegister();
 
   // while (not nr.isReady())  {
   //    delay(200);
   //    Serial.print(".");
   // }
-  Serial.println("\nNamino Rosso Interface started!");
+  // Serial.println("\nNamino Rosso Interface started!");
   // MFRC522 Setup
+  // Wire.begin();
+  // disable on SPI bus
+  // pinMode(NM_SS, OUTPUT);
+  // digitalWrite(NM_SS, HIGH);
+
+  // CS Reader Pin
+  pinMode(CS_PIN, OUTPUT);
+  digitalWrite(CS_PIN, HIGH);
+
+  spi = SPIClass(VSPI);
+  spi.begin(CLK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);        // Init SPI bus
   Serial.println("RFID RC522 Test");
+  digitalWrite(CS_PIN, LOW);
   mfrc522.PCD_Init(); // Init MFRC522 card
   Serial.println("Tap an RFID/NFC tag");
   Serial.println("NO TAG");
@@ -57,7 +99,7 @@ void loop() {
     return;
   }
   lastLoop = theTime;
-  nr.readAllRegister();
+  // nr.readAllRegister();
   // Check RFID Presence
   if (mfrc522.PICC_IsNewCardPresent()) { // new tag is available
     lastTagdRead = theTime;
--- PlatformIO/namino_rfid_access/include/main.h
+++ PlatformIO/namino_rfid_access/include/main.h
@@ -1,17 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <Arduino.h>
+#include "pins_arduino.h"
 #include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
 
-#define RST_PIN         17          // Configurable, see typical pin layout above
-
-#define SDA_PIN          5          // Configurable, see typical pin layout above
-#define SCK_PIN         18          // Configurable, see typical pin layout above
-#define MISO_PIN        19          // Configurable, see typical pin layout above
-#define MOSI_PIN        23          // Configurable, see typical pin layout above
+#define CS_PIN   16
+#define RST_PIN  18
+#define SCK_PIN  14
+#define MISO_PIN 17
+#define MOSI_PIN  9
 
 #define LOOP_PERIOD   500           // 500ms loop period
 #define SCREEN_ON    8000           //  s of screen on time
