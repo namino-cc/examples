df82f9220ba9d74bbebabc9b4b67f0bb97e4e429
Andrea Sansoe <asansoe@mect.it>
2023-09-28 16:10:20 +0200

    Namino Arancio RFID Magic Box Example: Handled Arduino Pins

---
+++ PlatformIO/namino_arancio_magic_box/include/main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// RC-522 SPI PINS
+#define CS_PIN      16
+#define RST_PIN     18
+#define SCK_PIN     14
+#define MISO_PIN    17
+#define MOSI_PIN     9
+
+// Arduino Pins for LED traffic light module
+#define GREEN_PIN    5
+#define YELLOW_PIN   4
+#define RED_PIN      3
+
+// Namino Out Channel for Light Indicators
+#define GREEN_LIGHT  1
+#define YELLOW_LIGHT 2
+#define RED_LIGHT    3
+#define WHITE_LIGHT  4
+
+// Namino Keylock Input
+#define KEYLOCK_IN   1
+
+// Loop interval
+#define LOOP_PERIOD        100          // 100ms loop period
+#define LINE_LEN            40          // Line Buffer Size
+#define TAG_LEN             16          // Maximum Tag Len
+#define KEYLOCK_ON_TIME    150          // Lock opening impulse duration
+#define BLINK_ON_TIME    20000          // Blink On Time  
+#define RESULT_TIME       6000          // Time to show Failure or Success
+
+// Function Protos
+void tagOK();
+void tagFailed();
+void setKeyLock(bool);
+bool setBlink(bool);
+
+#endif // MAIN_H
--- PlatformIO/namino_arancio_magic_box/src/main.cpp
+++ PlatformIO/namino_arancio_magic_box/src/main.cpp
@@ -2,28 +2,25 @@
 #include <Wire.h>
 #include <MFRC522.h>
 
-
-
 #ifdef NAMINO_ROSSO_BOARD
 #undef NAMINO_ROSSO_BOARD
 #endif
 
-#define CS_PIN   16
-#define RST_PIN  18
-#define SCK_PIN  14
-#define MISO_PIN 17
-#define MOSI_PIN  9
+#include "main.h"
 
 MFRC522 mfrc522(CS_PIN, RST_PIN);   // Create MFRC522 instance.
 MFRC522::MIFARE_Key key;
 
-// Loop interval
-#define LOOP_PERIOD   500           // 500ms loop period
-#define LINE_LEN    20              // Line Buffer Size
-
+// Time counters (in millis)
 unsigned long lastTagdRead = 0;
 unsigned long lastLoop = 0;
+unsigned long lastDoorOpened = 0;
+unsigned long startBlinkTime = 0;
+unsigned long startResultTime = 0;
+
+// Status Flags
 bool          readerOk = false;
+bool          blinkOn = false;
 
 
 void setup() {
@@ -49,6 +46,14 @@ void setup() {
   Serial.flush();
   delay(2000);
 
+  // I/O Pins
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(KEYLOCK_IN, INPUT);
+  digitalWrite(GREEN_PIN, HIGH);    
+  digitalWrite(RED_PIN, HIGH);    
+  digitalWrite(YELLOW_PIN, HIGH);    
 
   // Starting custom SPI
   Serial.println("Starting SPI Interface");
@@ -72,11 +77,19 @@ void setup() {
   else  {
     Serial.println("Error in Reader Self Test");
   }
+  // Starting
+  delay(2000);
+  digitalWrite(GREEN_PIN, LOW);    
+  digitalWrite(RED_PIN, LOW);    
+  digitalWrite(YELLOW_PIN, LOW);    
 }
 
 void loop() {
   unsigned long theTime = millis();
   char line[LINE_LEN + 1];
+  uint64_t    tagIDLow = 0;
+  uint64_t    tagIDHigh = 0;
+  byte        tagID[TAG_LEN];
 
   // limit loop period
   if (abs( (long long) (theTime - lastLoop)) < LOOP_PERIOD)  {
@@ -85,41 +98,118 @@ void loop() {
   lastLoop = theTime;
 
   // Check RFID Reader
-  if (readerOk)  {
-    // Check RFID Presence
-    if(mfrc522.PICC_IsNewCardPresent()) { 
-      // new tag is available
-      lastTagdRead = theTime;
-      Serial.println();
-      if (mfrc522.PICC_ReadCardSerial()) { // NUID has been readed
-        memset(line, 0, LINE_LEN);
-        MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-        strcat(line, "RFID/NFC Tag Type:");
-        Serial.print(line);
+  if (not readerOk)  {
+    Serial.println("No Reader Found!");
+    return;
+  }
 
-        strncpy(line, (char *) mfrc522.PICC_GetTypeName(piccType), LINE_LEN);
-        Serial.println(line);
+  // Switch Off Keylock
+  if (lastDoorOpened > 0 && KEYLOCK_ON_TIME < (theTime - lastDoorOpened) )  {
+    setKeyLock(false);
+    lastDoorOpened = 0;
+  }
 
-        // Get Tag ID
-        strcpy(line, "UID:");
-        Serial.print(line);
-        for (int i = 0; i < mfrc522.uid.size; i++) {
-          // Build Tag ID String
-          char byteStr[5];
-          sprintf(byteStr, " %02X", (int) (mfrc522.uid.uidByte[i]) ) ;
-          strcat(line, byteStr);
-        }
-        Serial.println(line);
-        mfrc522.PICC_HaltA(); // halt PICC
-        mfrc522.PCD_StopCrypto1(); // stop encryption on PCD
+  // Led Blinking
+  if (startBlinkTime > 0)  {
+      if (BLINK_ON_TIME < (theTime - startBlinkTime))  {
+        // Definitely stops the blinking
+        setBlink(false);
+        blinkOn = false;
+        startBlinkTime = 0;
       }
-    }
-    else  {
-      Serial.print(".");
+      else  {
+        blinkOn = setBlink(blinkOn);
+      }
+  }
+
+  // Result Display
+  if (startResultTime > 0)  {
+    if (RESULT_TIME < (theTime - startResultTime))  {
+      digitalWrite(GREEN_PIN, LOW);
+      digitalWrite(RED_PIN, LOW);
+      startResultTime = 0;
     }
   }
-  else  {
-    Serial.println("No Reader Found!");
+  // Check RFID Presence only if ended result time
+  if(startBlinkTime == 0 && mfrc522.PICC_IsNewCardPresent()) { 
+    // new tag is available
+    lastTagdRead = theTime;
+    Serial.println();
+    if (mfrc522.PICC_ReadCardSerial()) { // NUID has been readed
+      memset(line, 0, LINE_LEN);
+      MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+      strcat(line, "RFID/NFC Tag Type: ");
+      Serial.print(line);
+
+      strncpy(line, (char *) mfrc522.PICC_GetTypeName(piccType), LINE_LEN);
+      Serial.println(line);
+
+      // Get Tag ID
+      strcpy(line, "UID:");
+      Serial.print(line);
+      // UUID may be 4, 7 or 10 Bytes
+      memset(&(tagID[0]), 0, TAG_LEN);
+      tagIDLow = 0;
+      tagIDHigh = 0;
+      for (int i = 0; i < mfrc522.uid.size; i++) {
+        // Build Tag ID String
+        char byteStr[5];
+        sprintf(byteStr, " %02X", (int) (mfrc522.uid.uidByte[i]) ) ;
+        tagID[i] = mfrc522.uid.uidByte[i];
+        strcat(line, byteStr);
+      }
+      // Can copy read value byte per byte as the platform is "Little Endian"
+      memcpy(&tagIDLow, &(tagID[0]), sizeof(tagIDLow));
+      if (mfrc522.uid.size > 8)  {
+        memcpy(&tagIDHigh, &(tagID[sizeof(tagIDLow)]), sizeof(tagIDHigh));
+      }
+      else  {
+        tagIDHigh = 0;
+      }
+      Serial.printf("[%s] Len:[%d] Low:[%u] - High:[%u]\n", line, mfrc522.uid.size, tagIDLow, tagIDHigh);
+      mfrc522.PICC_HaltA();     // halt PICC
+      mfrc522.PCD_StopCrypto1(); // stop encryption on PCD
+      // Success Reading Tag
+      tagOK();
+    }
   }
 }
 
+void tagOK()
+{
+  setKeyLock(true);
+  // Update Door Opening Time Stamp
+  lastDoorOpened = millis();
+  startBlinkTime = lastDoorOpened;
+  startResultTime = lastDoorOpened;
+  digitalWrite(GREEN_PIN, HIGH);
+}
+
+void tagFailed()
+{
+  digitalWrite(RED_PIN, HIGH);
+  startResultTime = millis();
+}
+
+void setKeyLock(bool keyON)
+{
+  if (keyON)  {
+	  digitalWrite(RED_PIN, HIGH);    
+  }
+  else  {
+	  digitalWrite(RED_PIN, LOW);    
+  }
+  Serial.printf("Set KeyLock: [%d]\n", keyON);
+}
+
+bool setBlink(bool blinkON)
+{
+  if (blinkON)  {
+	  digitalWrite(YELLOW_PIN, HIGH);    
+  }
+  else  {
+	  digitalWrite(YELLOW_PIN, LOW);    
+  }
+  // Serial.printf("Set Led Blink: [%d]\n", blinkON);
+  return not blinkON;
+}
