153f7840f21590bdef5cb75d21ed42a55b066271
Andrea Sansoe <asansoe@mect.it>
2023-10-18 17:47:15 +0200

    Namino Maker Fair Rome 2023: Added Mect Suite TPAC Monitor Project

---
+++ PlatformIO/namino_home_maker_fair_rome_2023/TPAC/SensoriSmartHome/languages_it.ts
@@ -0,0 +1,206 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>page100</name>
+    <message>
+        <location filename="page100.ui" line="26"/>
+        <source>page100</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="52"/>
+        <location filename="page100.ui" line="161"/>
+        <location filename="page100.ui" line="290"/>
+        <location filename="page100.ui" line="811"/>
+        <location filename="page100.ui" line="1142"/>
+        <location filename="page100.ui" line="1280"/>
+        <location filename="page100.ui" line="1409"/>
+        <location filename="page100.ui" line="1516"/>
+        <source>00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="55"/>
+        <location filename="page100.ui" line="61"/>
+        <source>RTU_Temp</source>
+        <oldsource>Mirror_Temp</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="58"/>
+        <location filename="page100.ui" line="167"/>
+        <location filename="page100.ui" line="296"/>
+        <location filename="page100.ui" line="817"/>
+        <location filename="page100.ui" line="1148"/>
+        <location filename="page100.ui" line="1286"/>
+        <location filename="page100.ui" line="1415"/>
+        <location filename="page100.ui" line="1522"/>
+        <source>ATCMlabel widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="118"/>
+        <source>Temperature</source>
+        <oldsource>Temperatura</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="page100.ui" line="140"/>
+        <source>°C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="164"/>
+        <location filename="page100.ui" line="170"/>
+        <source>RTU_HR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="227"/>
+        <source>%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="249"/>
+        <source>Humidity %</source>
+        <oldsource>Umidità %</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="271"/>
+        <source>index</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="293"/>
+        <location filename="page100.ui" line="299"/>
+        <source>RTU_VOC</source>
+        <oldsource>Mirror_VOC</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="356"/>
+        <location filename="page100.ui" line="518"/>
+        <source>VOC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="814"/>
+        <location filename="page100.ui" line="820"/>
+        <source>RTU_C_PM2_5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="968"/>
+        <source>ppm</source>
+        <oldsource>ppb</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1145"/>
+        <location filename="page100.ui" line="1151"/>
+        <source>RTU_CO2_ppm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1261"/>
+        <source>PM 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1283"/>
+        <location filename="page100.ui" line="1289"/>
+        <source>RTU_C_PM1_0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1368"/>
+        <source>PM 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1412"/>
+        <location filename="page100.ui" line="1418"/>
+        <source>RTU_C_PM4_0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1475"/>
+        <source>PM 10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1519"/>
+        <location filename="page100.ui" line="1525"/>
+        <source>RTU_C_PM10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1230"/>
+        <location filename="page100.ui" line="1233"/>
+        <source>menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="372"/>
+        <location filename="page100.ui" line="441"/>
+        <location filename="page100.ui" line="537"/>
+        <location filename="page100.ui" line="606"/>
+        <location filename="page100.ui" line="675"/>
+        <location filename="page100.ui" line="717"/>
+        <location filename="page100.ui" line="920"/>
+        <source>ATCMled widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="422"/>
+        <source>T</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="491"/>
+        <source>HR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="767"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;CH&lt;span style=&quot; vertical-align:sub;&quot;&gt;4&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="901"/>
+        <location filename="page100.ui" line="990"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;CO&lt;span style=&quot; vertical-align:sub;&quot;&gt;2&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="1208"/>
+        <source>ATCMbutton widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="587"/>
+        <source>PM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="656"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;O&lt;span style=&quot; vertical-align:sub;&quot;&gt;3&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="792"/>
+        <source>PM 2.5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="page100.ui" line="877"/>
+        <location filename="page100.ui" line="1346"/>
+        <location filename="page100.ui" line="1390"/>
+        <location filename="page100.ui" line="1497"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;ug/m&lt;span style=&quot; vertical-align:super;&quot;&gt;3&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ PlatformIO/namino_home_maker_fair_rome_2023/TPAC/SensoriSmartHome/page500.h
@@ -0,0 +1,38 @@
+/**
+ * @file
+ *
+ * @section LICENSE
+ * Copyright Mect s.r.l. 2013
+ *
+ * @brief Main page
+ */
+#ifndef PAGE500_H
+#define PAGE500_H
+
+#include "pagebrowser.h"
+
+namespace Ui {
+class page500;
+}
+
+class page500 : public page
+{
+    Q_OBJECT
+    
+public:
+    explicit page500(QWidget *parent = 0);
+    ~page500();
+    virtual void reload(void);
+    virtual void updateData();
+    
+private slots:
+    void changeEvent(QEvent * event);
+    
+
+
+private:
+    Ui::page500 *ui;
+};
+
+#endif // PAGE500_H
+
