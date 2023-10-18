36df4890035f40b27b860e304ba635aaaeb5d901
Andrea Sansoe <asansoe@mect.it>
2023-10-18 17:42:30 +0200

    Namino Bianco RFID Magic Box Example: Fixed pin I/O layout

--- PlatformIO/namino_bianco_magic_box/include/main.h
+++ PlatformIO/namino_bianco_magic_box/include/main.h
@@ -5,10 +5,10 @@
 #define CS_PIN      48          // Chip Select  on Shared SPI Bus (RFID Reader)
 #define RST_PIN     18          // Reset pin for RFID Reader
 
-// Arduino Pins for LED Traffic light module
-#define GREEN_PIN    5
-#define YELLOW_PIN   4
-#define RED_PIN      3
+// Arduino 3.3V Pins for LED Traffic light module
+#define GREEN_PIN    40
+#define YELLOW_PIN   39
+#define RED_PIN      38
 
 // Box Lights (4M Relay is ACTIVE LOW!!)
 #define GREEN_LIGHT  42
@@ -19,7 +19,7 @@
 // Keylock Output on Magic Box (via Active HIGH relay)
 #define KEYLOCK_OUT  14
 
-// Keylock Input on Magic Box
+// Keylock Input on Magic Box (12V Input on PC Input block)
 #define KEYLOCK_IN   06
 
 // Loop interval & Timing constants
@@ -33,8 +33,8 @@
 #define LINE_LEN           256          // Line Buffer Size
 #define TAG_LEN             16          // Maximum Tag Len
 #define ITEM_LEN            32          // Item Display string len
-#define TAG_ITEMS           10          // Number of Tags handled
-#define WINNERS_TAG          5          // Number of Winners Tags
+#define TAG_ITEMS           20          // Number of Tags handled
+#define WINNERS_TAG         10          // Number of Winners Tags
 
 // Function Protos
 void selectWinners();
